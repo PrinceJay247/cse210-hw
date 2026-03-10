4de47c8a13f7715b1f9dd1409a730953efe57d9e
Prince Jacob Nathaniel <offixialprinciple2005@gmail.com>
2026-03-10 18:00:48 +0100

    Updated all 5 files with latest code

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+         {
+        List<int> numbers = new List<int>();
+        int input;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished ");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0 )
+            {
+                numbers.Add(input);
+            }
+        }
+
+        while (input != 0 );
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        int average = sum / numbers.Count;
+        Console.WriteLine($"The average is {average}");
+
+         
+       int max = numbers[0];
+       foreach(int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+         
+         Console.WriteLine($"The largest number is: {max}");
+
+
+        
+
+
+       
+
+        
+    }
     }
 }
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        {
+        Console.WriteLine("Welcome to the program!");
+
+        Console.Write("Please enter your name:");
+        string userName = Console.ReadLine();
+
+        Console.Write("Please enter your favorite number:");
+        int favNum = int.Parse(Console.ReadLine());
+
+        int squareNumber = favNum * favNum;
+
+        Console.WriteLine($"{userName}, the square of your number is {squareNumber}");
+    }
     }
 }
