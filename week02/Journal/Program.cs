aa3a809e93e59278456b474d67ba876c272ee38d
Prince Jacob Nathaniel <offixialprinciple2005@gmail.com>
2026-03-21 21:44:52 +0100

    implement fraction class with constructors, getters/setters and display methods

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,51 +1,33 @@
-using System;
-
-class Program
+ 
+public class Person
 {
-    static void Main(string[] args)
+    private string _title;
+    private string _firstName;
+    private string _lastName;
+   
+
+    public Person()
     {
-        Journal journal = new Journal();
-        bool running = true;
+        _title = "";
+        _firstName = "anonymus";
+        _lastName = "unknown";
 
-        while (running)
-        {
-            Console.WriteLine("Journal Menu:");
-            Console.WriteLine("1. Write a new entry");
-            Console.WriteLine("2. Display the journal");
-            Console.WriteLine("3. Save the journal to a file");
-            Console.WriteLine("4. Load the journal from a file");
-            Console.WriteLine("5. Quit");
-            Console.Write("Choose an option: ");
-
-            string choice = Console.ReadLine();
-            Console.WriteLine();
-
-            switch (choice)
-            {
-                case "1":
-                    journal.AddEntry();
-                    break;
-                case "2":
-                    journal.DisplayAll();
-                    break;
-                case "3":
-                    Console.Write("Enter filename to save: ");
-                    string saveFile = Console.ReadLine();
-                    journal.SaveToFile(saveFile);
-                    break;
-                case "4":
-                    Console.Write("Enter filename to load: ");
-                    string loadFile = Console.ReadLine();
-                    journal.LoadFromFile(loadFile);
-                    break;
-                case "5":
-                    running = false;
-                    Console.WriteLine("Goodbye!");
-                    break;
-                default:
-                    Console.WriteLine("Invalid option. Try again.\n");
-                    break;
-            }
-        }
     }
+
+    public Person(string first, string last )
+    {
+        _title = "";
+        _firstName = first;
+        _lastName = last;
+        
+    }
+
+    public Person(string first, string last, string title)
+    {
+        _title = title;
+        _firstName = first;
+        _lastName = last;
+    }
+
+   
 }
