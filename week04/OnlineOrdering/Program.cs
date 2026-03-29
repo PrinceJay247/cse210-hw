6a6b1b79ee328f1df9a7862963c9b29bd231323b
Prince Jacob Nathaniel <offixialprinciple2005@gmail.com>
2026-03-29 04:22:15 +0100

    Program, Comment,Video

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -1,167 +1,42 @@
 using System;
 using System.Collections.Generic;
 
-// PRODUCT CLASS
-class Product
-{
-    private string _name;
-    private string _productId;
-    private double _price;
-    private int _quantity;
-
-    public Product(string name, string productId, double price, int quantity)
-    {
-        _name = name;
-        _productId = productId;
-        _price = price;
-        _quantity = quantity;
-    }
-
-    public double GetTotalCost()
-    {
-        return _price * _quantity;
-    }
-
-    public string GetPackingInfo()
-    {
-        return $"{_name} (ID: {_productId})";
-    }
-}
-
-// ADDRESS CLASS
-class Address
-{
-    private string _street;
-    private string _city;
-    private string _state;
-    private string _country;
-
-    public Address(string street, string city, string state, string country)
-    {
-        _street = street;
-        _city = city;
-        _state = state;
-        _country = country;
-    }
-
-    public bool IsInUSA()
-    {
-        return _country.ToLower() == "usa";
-    }
-
-    public string GetFullAddress()
-    {
-        return $"{_street}\n{_city}, {_state}\n{_country}";
-    }
-}
-
- class Customer
-{
-    private string _name;
-    private Address _address;   
-
-    public Customer(string name, Address address)
-    {
-        _name = name;
-        _address = address;
-    }
-
-    public bool LivesInUSA()
-    {
-        return _address.IsInUSA();
-    }
-
-    public string GetName()
-    {
-        return _name;
-    }
-
-    public string GetAddress()
-    {
-        return _address.GetFullAddress();
-    }
-}
-
-// ORDER CLASS
-class Order
-{
-    private List<Product> _products = new List<Product>();
-    private Customer _customer;
-
-    public Order(Customer customer)
-    {
-        _customer = customer;
-    }
-
-    public void AddProduct(Product product)
-    {
-        _products.Add(product);
-    }
-
-    public double GetTotalCost()
-    {
-        double total = 0;
-
-        foreach (Product p in _products)
-        {
-            total += p.GetTotalCost();
-        }
-
-        // Shipping cost
-        if (_customer.LivesInUSA())
-            total += 5;
-        else
-            total += 35;
-
-        return total;
-    }
-
-    public string GetPackingLabel()
-    {
-        string label = "Packing Label:\n";
-
-        foreach (Product p in _products)
-        {
-            label += p.GetPackingInfo() + "\n";
-        }
-
-        return label;
-    }
-
-    public string GetShippingLabel()
-    {
-        return $"Shipping Label:\n{_customer.GetName()}\n{_customer.GetAddress()}";
-    }
-}
-
-// MAIN PROGRAM
 class Program
 {
     static void Main(string[] args)
     {
-        // ORDER 1 (USA)
-        Address address1 = new Address("123 Main St", "New York", "NY", "USA");
-        Customer customer1 = new Customer("John Doe", address1);
+         Video video1 = new Video("How to Code in C#", "John Dev", 600);
+        Video video2 = new Video("Encapsulation", "Jane Tech", 720);
+        Video video3 = new Video("Learn Classes Fast", "CodeMaster", 540);
 
-        Order order1 = new Order(customer1);
-        order1.AddProduct(new Product("Laptop", "P001", 800, 1));
-        order1.AddProduct(new Product("Mouse", "P002", 20, 2));
+         video1.AddComment(new Comment("Alice", "Great tutorial!"));
+        video1.AddComment(new Comment("Bob", "Very helpful."));
+        video1.AddComment(new Comment("Chris", "I learned a lot."));
 
-        // ORDER 2 (International)
-        Address address2 = new Address("45 Queen Rd", "London", "N/A", "UK");
-        Customer customer2 = new Customer("Jane Smith", address2);
+         video2.AddComment(new Comment("Dave", "Nice explanation."));
+        video2.AddComment(new Comment("Ella", "Clear and simple."));
+        video2.AddComment(new Comment("Frank", "Loved it!"));
 
-        Order order2 = new Order(customer2);
-        order2.AddProduct(new Product("Phone", "P003", 600, 1));
-        order2.AddProduct(new Product("Charger", "P004", 25, 3));
+         video3.AddComment(new Comment("Prince", "Awesome content."));
+        video3.AddComment(new Comment("Johnson", "Helped me understand classes."));
+        video3.AddComment(new Comment("Patience", "Thanks a lot!"));
 
-        // DISPLAY RESULTS
-        Console.WriteLine(order1.GetPackingLabel());
-        Console.WriteLine(order1.GetShippingLabel());
-        Console.WriteLine($"Total Cost: ${order1.GetTotalCost()}\n");
+         List<Video> videos = new List<Video> { video1, video2, video3 };
 
-        Console.WriteLine(order2.GetPackingLabel());
-        Console.WriteLine(order2.GetShippingLabel());
-        Console.WriteLine($"Total Cost: ${order2.GetTotalCost()}");
+         foreach (Video video in videos)
+        {
+            Console.WriteLine("Title: " + video.Title);
+            Console.WriteLine("Author: " + video.Author);
+            Console.WriteLine("Length (seconds): " + video.Length);
+            Console.WriteLine("Number of Comments: " + video.GetCommentCount());
+
+            Console.WriteLine("Comments:");
+            foreach (Comment comment in video.GetComments())
+            {
+                Console.WriteLine($"- {comment.Name}: {comment.Text}");
+            }
+
+            Console.WriteLine();  
+        }
     }
 }
